0d75395f7d491c5c1e21e95725b1b6bbc34ed632
Piet Geursen <pietgeursen@gmail.com>
2019-11-18 11:19:14 +0100

    Adds some docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,34 @@
+//! Causally sort a collection of messages.
+//!
+//! Returns messages sorted from newest to oldest.
+//!
+//! If `message b` includes a reference to `message a` then we say that `message b` _must_ have been
+//! published after `message a`, assuming these assumptions hold:
+//! - The hash function is not broken (Two different sets of bytes return the same hash.)
+//! - The person publishing `message b` has not guessed a valid hash of a message before it was
+//! published (extremely unlikely.)
+//! - The person publishing `message b` is not a time a traveller. 
+//!
+//! This function uses [daggy]() to build a [dag]() of references between messages and then
+//! topologically sorts them.
+//!
+//! If a message is not referenced by any messages you can expect it to be sorted to the start of
+//! the results (it is so new no one has referenced it yet). 
+//!
 use daggy::{Dag, NodeIndex, Walker};
 use petgraph::visit::Topo;
 use serde_json::Value;
 use ssb_multiformats::multihash::Multihash;
 use std::collections::HashMap;
 
-// If a message references another then the referencer must come afterwards causally.
-// TBD how to include "missing context"
-
 pub fn causal_sort(msgs: &[(Multihash, &[u8])]) -> Vec<Multihash> {
+    // Thought: Can we enumerate the iter and use the index as a key for one or both of the hashes?
     let (dag, _, node_to_hash) = msgs
         .iter()
         .map(|(key, msg)| {
             let value: Value = serde_json::from_slice(msg).unwrap();
             let mut refs = Vec::new();
-            // get all the links in each message.
+            // Recursively search through the object searching for Multihashes
             find_all_links(&value, &mut refs);
             (key, refs)
         })
@@ -36,7 +51,7 @@ pub fn causal_sort(msgs: &[(Multihash, &[u8])]) -> Vec<Multihash> {
                         .entry(reference.clone())
                         .or_insert_with(|| dag.add_node(1));
                     node_to_hash.entry(*ref_node).or_insert(reference.clone());
-                    dag.add_edge(key_node.clone(), *ref_node, 1).unwrap();
+                    dag.add_edge(key_node.clone(), *ref_node, 1).expect("The dag has a cycle. This is _VERY_ unexpected. Either the SHA256 hash function is broken, someone is a time traveller, or someone guessed a hash of a message before it was ever created. Most likely this module has a bug :)");
                 });
 
                 (dag, hash_to_node, node_to_hash)
@@ -56,7 +71,6 @@ pub fn causal_sort(msgs: &[(Multihash, &[u8])]) -> Vec<Multihash> {
                 .is_some()
         })
         .collect()
-    // tie break by a timestamp
 }
 
 fn find_all_links(obj: &Value, keys: &mut Vec<Multihash>) {
@@ -126,6 +140,43 @@ mod tests {
             (k2.clone(), v2.as_slice()),
             (k1.clone(), v1.as_slice()),
             (k3.clone(), v3.as_slice()),
+        ];
+        let sorted = causal_sort(&unsorted[..]);
+
+        assert_eq!(sorted.as_slice(), [k3,k2,k1])
+    }
+    #[test]
+    fn it_works_with_orphaned_messages() {
+        let k1 = Multihash::from_legacy(b"%rootBOK7pZikWM6aupei3PuE5ghRtFM44nrsX0FuBWY=.sha256")
+            .unwrap()
+            .0;
+        let v1_value = json!({
+            "previous":  "%1AfrBOK7pZikWM6aupei3PuE5ghRtFM44nrsX0FuBWY=.sha256",
+            "nested": {
+                "previous":  "%2AfrBOK7pZikWM6aupei3PuE5ghRtFM44nrsX0FuBWY=.sha256",
+                "arry": ["%3AfrBOK7pZikWM6aupei3PuE5ghRtFM44nrsX0FuBWY=.sha256", "%4AfrBOK7pZikWM6aupei3PuE5ghRtFM44nrsX0FuBWY=.sha256" ]
+            }
+        });
+        let v1 = to_vec(&v1_value).unwrap();
+
+        let k2 = Multihash::from_legacy(b"%reply1K7pZikWM6aupei3PuE5ghRtFM44nrsX0FuBWY=.sha256")
+            .unwrap()
+            .0;
+        let v2_value = json!({
+            "root":  "%rootBOK7pZikWM6aupei3PuE5ghRtFM44nrsX0FuBWY=.sha256",
+        });
+        let v2 = to_vec(&v2_value).unwrap();
+        let k3 = Multihash::from_legacy(b"%orphanK7pZikWM6aupei3PuE5ghRtFM44nrsX0FuBWY=.sha256")
+            .unwrap()
+            .0;
+        let v3_value = json!({
+        });
+        let v3 = to_vec(&v3_value).unwrap();
+
+        let unsorted = [
+            (k2.clone(), v2.as_slice()),
+            (k3.clone(), v3.as_slice()),
+            (k1.clone(), v1.as_slice()),
         ];
         let sorted = causal_sort(&unsorted[..]);
 
